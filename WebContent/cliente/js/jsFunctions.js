64dbc137ab62d3baaf07862a96d2a844de9dd27f
Monzon <Monzon@DESKTOP-NGNHNN7>
2020-05-08 21:28:27 +0200

    cambios en el index

--- WebContent/cliente/js/jsFunctions.js
+++ WebContent/cliente/js/jsFunctions.js
@@ -1,40 +1,48 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    createHTML();
+  createHTML();
 
-  })
-  
-  function createHTML() {
-   
-    $.ajax({ 
-      url: 'http://localhost:8080/Komikilandia_IM/ApiComics',
-      dataType: 'json', 
-  
-      success: function (myJsonObject) {
+})
 
-        console.log(myJsonObject);
+function createHTML() {
 
-        var rowOpenHTML = "<div class='row justify-content-center'>"
-        var colOpenHTML = "<div class='col-lg-3 m-1'>"
-        var myHTMLcode = "";
-  
-        myHTMLcode=rowOpenHTML;
-        
-        for (let i = 0; i < myJsonObject.length; i++) {
-  
-            myHTMLcode += colOpenHTML;  
-  
-            myHTMLcode += "<div class='card h-100 text-center'> <div class='card-body'><h4 class='card-title'>" + myJsonObject[i].titulo + "</h4> <img class='card-img-bottom' src='" + myJsonObject[i].imagen + "'></div></div></div>";
-          }
-        myHTMLcode += "</div>";
-        document.getElementById("main").innerHTML += myHTMLcode;
-          
+  $.ajax({
+    url: 'http://localhost:8080/Komikilandia_IM/ApiComics',
+    dataType: 'json',
+
+    success: function (myJsonObject) {
+
+      console.log(myJsonObject);
+
+      var rowOpenHTML = "<div class='row justify-content-center'>"
+      var colOpenHTML = "<div class='col-lg-3 m-1'>"
+      var myHTMLcode = "";
+
+      myHTMLcode = rowOpenHTML;
+
+      for (let i = 0; i < myJsonObject.length; i++) {
+
+        myHTMLcode += colOpenHTML;
+
+        myHTMLcode += '<div class="card" style=" width: 18rem; ">\
+            <img src="'+ myJsonObject[i].imagen + '" class="card-img-top" alt="..." height="439">\
+            <div class="card-body text-center">\
+                <h5 class="card-title">'+ myJsonObject[i].nombre + '</h5>\
+                <p class="card-text">'+ myJsonObject[i].titulo + '</p>\
+                <a href="#" class="btn btn-info" data-toggle="modal" data-target="#id'+ myJsonObject[i].id + '">Más info</a>\
+            </div>\
+            <a href="#" class="btn btn-secondary"><img src="img/dedito.png" class=" " width="25%"> : '+ myJsonObject[i].num_likes + '</a>\
+        </div></div>';
+      }
+
+      myHTMLcode += "</div>";
+      document.getElementById("main").innerHTML += myHTMLcode;
+
 
     },
-      error: function (xhr) {
-        alert("AJAX error : " + xhr.status + " " + xhr.statusText);
-      }
-    });
-  
-  
-  }
-  
+    error: function (xhr) {
+      alert("AJAX error : " + xhr.status + " " + xhr.statusText);
+    }
+  });
+
+
+}
