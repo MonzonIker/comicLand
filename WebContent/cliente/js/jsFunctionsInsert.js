8b4ff7580c41b71fb9011c92dee0585647ec99f5
Monzon <Monzon@DESKTOP-NGNHNN7>
2020-05-22 20:52:02 +0200

    ultimo commit de LMAR antes del video

--- WebContent/cliente/js/jsFunctionsInsert.js
+++ WebContent/cliente/js/jsFunctionsInsert.js
@@ -1,68 +1,62 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    
+
     rellenarGeneros();
 
     document.getElementById('confirmar').addEventListener('click', insertar());
-  
-  })
 
-  function rellenarGeneros() {
+})
+
+function rellenarGeneros() {
 
     $.ajax({
-      url: 'http://localhost:8081/Komikilandia_IM/ApiGeneros',
-      dataType: 'json',
-  
-      success: function (myJsonObject) {
-  
-        /* para comprobar el contenido del objetoJsaon */
-        console.log(myJsonObject);
-  
-       /* variables que contendran nuestro html a insertar */
-       var myHTMLcode = "";
-  
-        /* recorremos todo el objetoJson y rellenamos y añadimos cards con la infrmacion de cada objeto */
-        for (let i = 0; i < myJsonObject.length; i++) {
-            
-            myHTMLcode += '<option value="'+ myJsonObject[i].id +'">'+ myJsonObject[i].nombre + '</option>';
-          
+        url: 'http://localhost:8081/Komikilandia_IM/ApiGeneros',
+        dataType: 'json',
+
+        success: function (myJsonObject) {
+            console.log(myJsonObject);
+            var myHTMLcode = "";
+            for (let i = 0; i < myJsonObject.length; i++) {
+
+                myHTMLcode += '<option value="' + myJsonObject[i].id + '">' + myJsonObject[i].nombre + '</option>';
+
+            }
+
+            document.getElementById("genero_id").innerHTML += myHTMLcode;
+
+        },
+        error: function (xhr) {
+            alert("AJAX error : " + xhr.status + " " + xhr.statusText);
         }
-  
-        document.getElementById("genero_id").innerHTML += myHTMLcode;
-
-      },
-      error: function (xhr) {
-        alert("AJAX error : " + xhr.status + " " + xhr.statusText);
-      }
     });
-  
-  
-  }
 
-  function insertar() {
 
-var nombre = document.getElementById("nombre").value;
-  var titulo = document.getElementById("titulo").value;
-  var num = document.getElementById("num").value;
-  var fecha_publicacion = document.getElementById("fecha_publicacion").value;
-  var imagen = document.getElementById("imagen").value;
-  var num_likes = document.getElementById("num_likes").value;
-  var genero_id = document.getElementById("genero_id").value;
+}
 
-  var comic = { "nombre": nombre, "titulo": titulo, "num": num, "fecha_publicacion": fecha_publicacion, "imagen": imagen, "num_likes": num_likes, "genero_id": genero_id };
-  
+function insertar() {
+
+    var nombre = document.getElementById("nombre").value;
+    var titulo = document.getElementById("titulo").value;
+    var num = document.getElementById("num").value;
+    var fecha_publicacion = document.getElementById("fecha_publicacion").value;
+    var imagen = document.getElementById("imagen").value;
+    var num_likes = document.getElementById("num_likes").value;
+    var genero_id = document.getElementById("genero_id").value;
+
+    var comic = { "nombre": nombre, "titulo": titulo, "num": num, "fecha_publicacion": fecha_publicacion, "imagen": imagen, "num_likes": num_likes, "genero_id": genero_id };
+
     $.ajax({
-      type: 'POST',
-      data: { 'comic': JSON.stringify(comic) },
-      url: 'http://localhost:8081/Komikilandia_IM/ApiInsertComic',
-      dataType: 'text',
-      success: function (response, status, xhr) {
-        if (xhr.status == 200) {
-          alert("Comic insertado " + xhr.statusText);
-          window.location.href = "index.html";
+        type: 'POST',
+        data: { 'comic': JSON.stringify(comic) },
+        url: 'http://localhost:8081/Komikilandia_IM/ApiInsertComic',
+        dataType: 'text',
+        success: function (response, status, xhr) {
+            if (xhr.status == 200) {
+                alert("Comic insertado " + xhr.statusText);
+                window.location.href = "index.html";
+            }
+        },
+        error: function (xhr) {
+            alert("An AJAX error occured: " + xhr.status + " " + xhr.statusText);
         }
-      },
-      error: function (xhr) {
-        alert("An AJAX error occured: " + xhr.status + " " + xhr.statusText);
-      }
     });
-  }
+}
